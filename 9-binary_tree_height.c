1c9071c5b589f9205c09a9bd124e78e464708bb9
kurves <kavssimply@gmail.com>
2024-03-26 05:38:52 -0700

    funtion that determines binary height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+#include "stdio.h"
+
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tre
+ * Description: function that gets binary height
+ * Return: int or null
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+	
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
+}
