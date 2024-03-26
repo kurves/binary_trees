a6d3b7d4ca0073e8631126809defd8b4d073d204
kurves <kavssimply@gmail.com>
2024-03-26 07:28:33 -0700

    function that checks if a binary tree is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,57 @@
+#include <math.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+#include "stdio.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Description: funtion to check if binary perfect
+ * Return: int or null
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+	
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
+ * Description: funtion toc heck size binary
+ * Return: Size of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ * Description: check if binary is perfec
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height = binary_tree_height(tree);
+	size_t size = binary_tree_size(tree);
+	size_t max_size = (size_t)pow(2, height) - 1;
+	
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (size == max_size);
+}
