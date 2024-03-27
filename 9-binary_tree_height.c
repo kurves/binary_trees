7c5e46ad7d32ddf1d4f70c32a3083f74839c43f1
kurves <kavssimply@gmail.com>
2024-03-27 02:43:41 -0700

    function that measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,14 +17,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (left_height > right_height)
+	if (tree)
 	{
-		return (left_height + 1);
+		left_height = tree->left ? 1 +  binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return((left_height > right_height ? left_height : right_height));
 	}
-	else
-	{
-		return (right_height + 1);
-	}
+	return (0);
 }
