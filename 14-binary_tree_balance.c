3930d5da12ebfd5f45d113012b33aefa1dc2f91a
kurves <kavssimply@gmail.com>
2024-03-27 05:51:17 -0700

    function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 #include "stdio.h"
+/**
+ * max - Returns the maximum of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Description: getting maix int
+ * Return: The maximum of a and b
+ */
+int max(int a, int b)
+{
+	return (a > b ? a : b);
+}
+
 
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -16,13 +28,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree)
-	{
-		left_height = tree->left ? 1 +  binary_tree_height(tree->left) : 0;
-		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((left_height > right_height ? left_height : right_height));
-	}
-	return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (1 + max(left_height, right_height));
 }
 
 
