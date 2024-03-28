83b4dbc3a98a47237508c23e436d94b86539caa2
kurves <kavssimply@gmail.com>
2024-03-28 04:28:53 -0700

    function that searches for a value in a Binary Search Tree

---
+++ 113-bst_search.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+#include "stdio.h"
+
+/**
+ * bst_search - Searches for a value in a Binary Search Tree
+ * @tree: Pointer to the root node of the BST to search
+ * @value: Value to search in the tree
+ * Description:search value in bts
+ * Return: Pointer to the node containing the value, or NULL if not found
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+	{
+		return NULL;
+	}
+	if (tree->n == value)
+	{
+		return (bst_t *)tree;
+	}
+	if (value < tree->n)
+	{
+		return bst_search(tree->left, value);
+	}
+	else
+	{
+		return bst_search(tree->right, value);
+	}
+}
