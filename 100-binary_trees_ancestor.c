4432bddd1cfe0f20669cf2f4a90f4a15ee976026
kurves <kavssimply@gmail.com>
2024-03-29 06:21:43 -0700

    fix betty coding style

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,10 +9,11 @@
  * Description : finding the common ancestor
  * Return: Pointer to the lowest common ancestor node, or NULL if not found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				const binary_tree_t *second)
 {
 	const binary_tree_t *temp1 = first, *temp2 = second;
-	
+
 	if (first == NULL || second == NULL)
 	{
 		return (NULL);
